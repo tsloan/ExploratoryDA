2322aae2d4bf18df93411c195ba3053af2fd05d7
tsloan <terry@sloandrums.com>
2014-08-24 01:29:49 +0100

    successful plot creation

--- Project2/plot3.R
+++ Project2/plot3.R
@@ -21,29 +21,29 @@ library(ggplot2)
 #############################################################################
 DataDir <-"data"
 FileURL <- 
-  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 
 if (!file.exists(DataDir)){
-  print("Creating directory for downloading data ...")
-  dir.create(DataDir)
-  ###########################################################################
-  ## Download the file into the Create directory 
-  ###########################################################################
-  download.file(FileURL, 
-                destfile = paste("./",DataDir,"/Dataset.zip",sep=""))
-  ###########################################################################
-  ## Add a file that contains the time and date the data is downloaded 
-  ###########################################################################
-  dateFileCon<-file("./data/DateDataDownloaded","w")
-  dateDownloaded <- date()
-  write(dateDownloaded,dateFileCon)
-  close(dateFileCon)
-  ###########################################################################
-  ## Unzip the archive into ./data
-  ###########################################################################
-  setwd(DataDir)
-  unzip("Dataset.zip")
-  setwd("..")
+    print("Creating directory for downloading data ...")
+    dir.create(DataDir)
+    ###########################################################################
+    ## Download the file into the Create directory 
+    ###########################################################################
+    download.file(FileURL, 
+                  destfile = paste("./",DataDir,"/Dataset.zip",sep=""))
+    ###########################################################################
+    ## Add a file that contains the time and date the data is downloaded 
+    ###########################################################################
+    dateFileCon<-file("./data/DateDataDownloaded","w")
+    dateDownloaded <- date()
+    write(dateDownloaded,dateFileCon)
+    close(dateFileCon)
+    ###########################################################################
+    ## Unzip the archive into ./data
+    ###########################################################################
+    setwd(DataDir)
+    unzip("Dataset.zip")
+    setwd("..")
 } # end of data download
 
 ##############################################################################
@@ -51,66 +51,80 @@ if (!file.exists(DataDir)){
 ## are in the data directory
 ##############################################################################
 print(
-  "Checking summarySCC_PM25.rds and Source_Classification_Code.rds exist ...")
+    "Checking summarySCC_PM25.rds and Source_Classification_Code.rds exist ...")
 
 dirlist<-dir(DataDir)
 if (("summarySCC_PM25.rds" %in% dirlist) 
     && ("Source_Classification_Code.rds" %in% dirlist)){
-  print("Data files exist, reading data ....")
-  #########################################################################
-  ## Read the data
-  #########################################################################
-  
-  ## This first line will likely take a few seconds. Be patient!
-  NEI <- readRDS(paste(DataDir,"summarySCC_PM25.rds",sep="/"))
-  SCC <- readRDS(paste(DataDir,"Source_Classification_Code.rds",sep="/"))
-  
-  ########################################################################
-  ## Create a data frame with the yearly total PM2.5 emissions 
-  ## for each source type ie. point, nonpoint, onroad, nonroad
-  ## for Baltimore, Maryland (fips == "24510") between 1999 and 2008.
-  ########################################################################
-  
-  Baltimore <- with ( NEI, (fips == "24510") )
-  
-#  print("Calculating yearly PM2.5 totals for Baltimore")
-#  PM25YearTotal <- 
-#    data.frame( year=c(1999,2002,2005,2008), 
-#                stype=c("POINT", "NONPOINT", "ONROAD", "NONROAD"),
-#                total=NA,stringsAsFactors=FALSE)
-  
-#  for (y in PM25YearTotal$year){
-#    for (st in PM25YearTotal$stype){
-        ## Get entries for the Baltimore emissions for each source type in
-        ## a particular year
-#        Baltimore <- with ( NEI, (fips == "24510" & year == y & type == st) )
-        ## set the mask for the PM25YearTotal$year and
-        ## PM25YearTotal$stype combination
-#        year.stype <- with(PM25YearTotal, (year == y & stype == st) )
-        ## sum the emission for each source type and year in Baltimore
-#        PM25YearTotal[year.stype,]$total = sum(NEI[Baltimore,]$Emissions)
-#    } # end of source for
-#  } # end of year for
-  
-  ########################################################################
-  ## Create a bar plot  
-  ########################################################################
-  
-#  png(file="plot3.png",width = 480, height = 480, units = "px")    
-#  with(PM25YearTotal,
-#       barplot(total,
-#               main =
-#                 "Total yearly PM2.5 emissions  
-#                  from all sources in Baltimore, Maryland, U.S.A.",
-#               ylab="Number of tons of PM2.5",
-#               xlab = "Year",
-#               names.arg=year
-#       ) # end barplot
-#  ) # end with
-#  dev.off()
-  
+    print("Data files exist, reading data ....")
+    #########################################################################
+    ## Read the data
+    #########################################################################
+    
+    ## This first line will likely take a few seconds. Be patient!
+    NEI <- readRDS(paste(DataDir,"summarySCC_PM25.rds",sep="/"))
+    SCC <- readRDS(paste(DataDir,"Source_Classification_Code.rds",sep="/"))
+    
+    
+    ########################################################################
+    ## Create a data frame with the yearly total PM2.5 emissions 
+    ## for each source type ie. point, nonpoint, onroad, nonroad
+    ## for Baltimore, Maryland (fips == "24510") between 1999 and 2008.
+    ########################################################################
+    
+    # initialise the data frame to hold the emissions totals
+    PM25YearTotal <- 
+        data.frame( character(), character(), numeric(), 
+                    stringsAsFactors=FALSE)
+    # initialise the column names
+    header <- c("year","src.type","total")
+    names(PM25YearTotal) <- header
+    
+    years <- c("1999","2002","2005","2008")
+    src.type <- c("POINT", "NONPOINT", "ON-ROAD", "NON-ROAD")
+    
+    #######################################################################
+    ## calculate the emissions for each year/source type combination
+    ## and populate the data frame
+    #######################################################################
+    
+    ## extract the Baltimore data
+    Baltimore <- NEI[NEI$fips == "24510",]
+    
+    ## Loop over the source type/year combination to get the total
+    ## emissions for the each
+    print("Generating the totals")
+    for (y in years){
+        for (st in src.type){
+            src.type.year <- with(Baltimore, (year == y & type == st) )
+            
+            ## create a new containing the sum of the emissions 
+            newrow <- data.frame(y, st, 
+                                 sum(Baltimore[src.type.year,]$Emissions), 
+                                 stringsAsFactors=FALSE)
+            names(newrow) <- header
+            # append the new row
+            PM25YearTotal<- rbind( PM25YearTotal, newrow )
+        } # end of source for
+    } # end of year for
+    
+    ########################################################################
+    ## Create the plot  
+    ########################################################################
+    print("Creating the plot")
+    png(file="plot3.png",width = 720, height = 480, units = "px")    
+    pl<- qplot(data=PM25YearTotal,
+               year, total,
+               facets=.~src.type,
+               ylab="Number of tons of PM2.5",
+                main = "Total yearly PM2.5 emissions for each source 
+                    in Baltimore, Maryland, U.S.A."
+             )
+    print(pl)
+    dev.off()
+    
 } # end of file plotting
 
 ##############################################################################
-## End of plot2.R
+## End of plot3.R
 ##############################################################################
